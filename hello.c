6f1adc33d16b9de359029368f17bcc63baffcec1
Raul Roa <raul@localstaffingllc.com>
2020-09-16 09:01:14 -0400

    Adds comments

--- hello.c
+++ hello.c
@@ -3,10 +3,13 @@
 
 int main(int argc, char * argv[])
 {
+    // Addition variable definition
     int x = 2;
     int y = 1;
 
     printf("Hello from C\n");
+
+    // Perform addition and print result
     printf("%d + %d = %d\n", x, y, add(x, y));
     return 0;
 }
