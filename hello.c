70c4c0a6b929aba2f1b99a56d2746767c3477701
Raul Roa <raul@localstaffingllc.com>
2020-09-16 08:34:49 -0400

    Adds math lib

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "pmath.h"
 
 int main(int argc, char * argv[])
 {
+    int x = 2;
+    int y = 1;
+
     printf("Hello from C\n");
+    printf("%d + %d = %d\n", x, y, add(x, y));
     return 0;
 }
