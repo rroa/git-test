9627bcc72da8098ca40356acb386c33bccacd0c0
Raúl Roa <raul.roa@gmail.com>
2020-09-16 09:28:09 -0400

    Adds division (#2)
    Co-authored-by: Raul Roa <raul@localstaffingllc.com>

--- hello.c
+++ hello.c
@@ -4,14 +4,15 @@
 int main(int argc, char * argv[])
 {
     // Addition variable definition
-    int x = 2;
-    int y = 1;
+    int x = 6;
+    int y = 3;
 
     printf("Hello from C\n");
 
     // Perform math operations and print result
     printf("%d + %d = %d\n", x, y, add(x, y));
     printf("%d - %d = %d\n", x, y, sub(x, y));    
-    printf("%d x %d = %d\n", x, y, multi(x, y));    
+    printf("%d x %d = %d\n", x, y, multi(x, y));   
+    printf("%d / %d = %d\n", x, y, div(x, y));    
     return 0;
 }
